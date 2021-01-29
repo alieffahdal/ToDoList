f1b120e5939194b47824b12a05fc1883971b5542
alieffahdal <alieffahdal@gmail.com>
2021-01-29 22:02:01 +0800

    semabarang

---
+++ tempData.js
@@ -0,0 +1,72 @@
+// Penyimpanan sebelum di konekkan di firebase
+
+
+export default tempData = [
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Fight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true    
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Erreands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Plan Weekend Camping Trip",
+                completed: true
+            },
+            {
+                title: "Run 2 Miles",
+                completed: true
+            },
+            {
+                title: "Make A Video",
+                completed: true
+            },
+            {
+                title: "Take Dog for a Walk",
+                completed: true
+            }
+
+        ]
+    },
+    {
+        name: "Birthday Party",
+        color: "#595BD9",
+        todos: [
+            {
+                title: "Get Ballons",
+                completed: false
+            },
+            {
+                title: "Send Invitations",
+                completed: false
+            },
+            {
+                title: "Make Dinner Reservation",
+                completed: true
+            }
+        ]
+    }
+]
